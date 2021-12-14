449e53d2834c884585e204430f25d5eed4b09dc1
vladimir <vladimirsok420@list.ru>
2021-12-14 18:14:56 +0300

    dz

--- src/skypro/Main.java
+++ src/skypro/Main.java
@@ -4,5 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println(upperFullName);
+
+
+        String result = fullName.replace(" ", " ; ");
+        System.out.println(result);
+
+
+        String fullName1 = "Иванов Семён Семёнович";
+        String validFullName1 = fullName1.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника — " +validFullName1);
+
+
     }
 }
